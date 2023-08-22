d821757902405cc7528a9c2ce41009bf6ff86c71
Brendan Woo <woo.brendan@gmail.com>
2023-08-22 14:34:34 -0400

    comment out old routes, import websocket component and pass to home route

--- podium-helper/src/App.js
+++ podium-helper/src/App.js
@@ -6,22 +6,24 @@ import NavBar from "./components/NavBar";
 import Results from "./components/Results/ResultsHistory";
 import RecentPodium from "./components/Results/RecentPodium";
 import EntryManager from "./components/EntryManager/EntryManager";
+import USACWebSocket from "./components/websocket";
 
 function App() {
-  return (
-    <Router>
-      <NavBar />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Podium />} />
+    return (
+        <Router>
+            <NavBar />
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<USACWebSocket />} />
+                    {/* <Route path="/" element={<Podium />} />
           <Route path="/competitors" element={<DriverSearch />} />
           <Route path="/results" element={<Results />} />
           <Route path="/recent" element={<RecentPodium />} />
-          <Route path="/entryManager" element={<EntryManager />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+          <Route path="/entryManager" element={<EntryManager />} /> */}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
