b23c95bf7e9e89b0cb578b9e0be4845a01226d55
Brendan Woo <woo.brendan@gmail.com>
2023-08-23 17:27:41 -0400

    fix import for readWSfile and fix error of filter function written as filtered

--- Socket_Test/getEntries.js
+++ Socket_Test/getEntries.js
@@ -1,9 +1,9 @@
-import { readWSFile } from "./simulate_socket";
+import { readWSFile } from "./simulate_socket.js";
 
 const getEntries = () => {
     const messages = readWSFile();
 
-    const filtered = messages.filtered(
+    const filtered = messages.filter(
         (message) => message.command === "$USAC:ENTRY",
     );
     console.log(filtered);
