f2cbd626b35cf2a0ab1b7192de069d6fadff3936
Brendan Woo <woo.brendan@gmail.com>
2023-08-24 13:50:05 -0400

    add in series value that grabs the runName and gets the series string, pass that to shorten the value

--- Socket_Test/getEntries.js
+++ Socket_Test/getEntries.js
@@ -1,12 +1,20 @@
 import { readWSFile } from "./simulate_socket.js";
+import { shortenName } from "../podium-helper/src/functions/helperFunc.js";
 
 const getEntries = async () => {
     const messages = await readWSFile();
 
+    const seriesList = messages.filter((message) => message.command === "$R");
+
+    const series = seriesList[0].runName.split(" R")[0];
+
+    const seriesShortHand = shortenName(series);
+    console.log("series", seriesShortHand);
+
     const filtered = messages.filter(
         (message) => message.command === "$USAC:ENTRY",
     );
-    console.log(filtered);
+    // console.log(filtered);
 };
 
 getEntries();
