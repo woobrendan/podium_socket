ed0d8478e96346be6904d863f7e6e378affc78ff
Brendan Woo <woo.brendan@gmail.com>
2023-08-25 14:24:39 -0400

    shortenName func and move to entryFunc file

--- Socket_Test/getEntries.js
+++ Socket_Test/getEntries.js
@@ -1,4 +1,5 @@
 import { readWSFile } from "./simulate_socket.js";
+import { shortenName } from "./entryFuncs.js";
 
 const getEntries = async () => {
     try {
@@ -8,6 +9,7 @@ const getEntries = async () => {
             (message) => message.command === "$R",
         );
 
+        // string runname is "Fanatec GT World Challenge Race 1", remove Race 1 from string
         const series = seriesList[0].runName.split(" R")[0];
 
         const seriesShortHand = shortenName(series);
@@ -17,22 +19,10 @@ const getEntries = async () => {
                 message.command === "$USAC:ENTRY" &&
                 message.series === seriesShortHand,
         );
-        console.log(filtered);
+        console.log(filtered[0]);
     } catch (err) {
         console.log("Error getting entries", error);
     }
 };
 
 getEntries();
-
-const shortenName = (seriesName) => {
-    const obj = {
-        "Fanatec GT World Challenge America": "FGTWCA",
-        "GT America": "GTA",
-        "TC America": "TCAM",
-        "Pirelli GT4 America": "GT4 America",
-        "Toyota GR Cup": "GR Cup",
-        "Intercontinental GT Challenge": "IGTC",
-    };
-    return obj[seriesName];
-};
