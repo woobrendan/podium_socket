8cbb92a7b15a9b56d51ec5f526c463e64e656282
Brendan Woo <woo.brendan@gmail.com>
2023-08-25 14:17:50 -0400

    wrap logic in try catch block, fix shorthand for shortenname on GTWCA to be FTGWCA

--- Socket_Test/getEntries.js
+++ Socket_Test/getEntries.js
@@ -1,27 +1,33 @@
 import { readWSFile } from "./simulate_socket.js";
 
 const getEntries = async () => {
-    const messages = await readWSFile();
+    try {
+        const messages = await readWSFile();
 
-    const seriesList = messages.filter((message) => message.command === "$R");
+        const seriesList = messages.filter(
+            (message) => message.command === "$R",
+        );
 
-    const series = seriesList[0].runName.split(" R")[0];
+        const series = seriesList[0].runName.split(" R")[0];
 
-    const seriesShortHand = shortenName(series);
+        const seriesShortHand = shortenName(series);
 
-    const filtered = messages.filter(
-        (message) =>
-            message.command === "$USAC:ENTRY" &&
-            message.series === seriesShortHand,
-    );
-    console.log(filtered);
+        const filtered = messages.filter(
+            (message) =>
+                message.command === "$USAC:ENTRY" &&
+                message.series === seriesShortHand,
+        );
+        console.log(filtered);
+    } catch (err) {
+        console.log("Error getting entries", error);
+    }
 };
 
 getEntries();
 
 const shortenName = (seriesName) => {
     const obj = {
-        "Fanatec GT World Challenge America": "GTWCA",
+        "Fanatec GT World Challenge America": "FGTWCA",
         "GT America": "GTA",
         "TC America": "TCAM",
         "Pirelli GT4 America": "GT4 America",
