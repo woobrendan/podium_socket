80df8329f4b2223c4c4af4781ff476b8b739b644
Brendan Woo <woo.brendan@gmail.com>
2023-08-28 14:23:21 -0400

    Create base template for getting final results

---
+++ Socket_Test/getFinal.js
@@ -0,0 +1,14 @@
+//** Once command goes to C(?) for checkered, push final messages into an arr, once list === length of entry list */
+const getFinal = (resultArr, series) => {};
+
+// const {
+//     event,
+//     series,
+//     date,
+//     fastLap,
+//     result1,
+//     result2,
+//     result3,
+//     result4,
+//     hardCharger,
+//   } = results;
