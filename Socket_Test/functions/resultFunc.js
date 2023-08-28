25102575d16ae4e2824cf62bc7f45daa430c6101
Brendan Woo <woo.brendan@gmail.com>
2023-08-28 16:28:43 -0400

    refactor handlClassPodium to use placementBuilder to build out result for each placement

--- Socket_Test/functions/resultFunc.js
+++ Socket_Test/functions/resultFunc.js
@@ -1,22 +1,37 @@
+//** Build out result(x) creating entry info for each placement, and setting class */
 const handleClassPodium = (result) => {
     const { firstPlace, secondPlace, thirdPlace } = result;
 
-    const firstDrivers = firstPlace.drivers;
-    const secondDriers = secondPlace.drivers;
-    const thirdDrivers = thirdPlace.drivers;
-
-    const finResult = {
+    return {
         class: result.class,
-        firstPlace: {
-            driver1: `${firstDrivers[0].firstname} ${firstDrivers[0].lastName}`,
-            ...(firstDrivers[1]
-                ? {
-                      driver2: `${firstDrivers[1].firstname} ${firstDrivers[1].lastName}`,
-                  }
-                : {}),
-            vehicle: firstPlace.vehicle,
-            team: firstPlace.teamName,
-            number: firstPlace.number,
-        },
+        firstPlace: { ...placementBuilder(firstPlace) },
+        ...(secondPlace
+            ? { secondPlace: { ...placementBuilder(secondPlace) } }
+            : {}),
+        ...(thirdPlace
+            ? { thirdPlace: { ...placementBuilder(thirdPlace) } }
+            : {}),
+    };
+};
+
+//** Take result for 1st, 2nd or 3rd and create object with entry information in shape expected for table */
+const placementBuilder = (placement) => {
+    const { vehicle, teamName, number, drivers } = placement;
+
+    return {
+        driver1: `${drivers[0].firstName} ${drivers[0].lastName}`,
+        ...(drivers[1]
+            ? {
+                  driver2: `${drivers[1].firstname} ${drivers[1].lastName}`,
+              }
+            : {}),
+        ...(drivers[2]
+            ? {
+                  driver3: `${drivers[2].firstname} ${drivers[2].lastName}`,
+              }
+            : {}),
+        vehicle,
+        team: teamName,
+        number,
     };
 };
