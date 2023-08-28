ea00c5210246d937470bf09de8cd6e563238a0cd
Brendan Woo <woo.brendan@gmail.com>
2023-08-28 16:15:02 -0400

    add logic to dynamically add information for second driver and for number

--- Socket_Test/functions/resultFunc.js
+++ Socket_Test/functions/resultFunc.js
@@ -9,8 +9,14 @@ const handleClassPodium = (result) => {
         class: result.class,
         firstPlace: {
             driver1: `${firstDrivers[0].firstname} ${firstDrivers[0].lastName}`,
+            ...(firstDrivers[1]
+                ? {
+                      driver2: `${firstDrivers[1].firstname} ${firstDrivers[1].lastName}`,
+                  }
+                : {}),
             vehicle: firstPlace.vehicle,
             team: firstPlace.teamName,
+            number: firstPlace.number,
         },
     };
 };
