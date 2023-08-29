db6b2a6459e58b6d13b28933956a4b3b0c695499
Brendan Woo <woo.brendan@gmail.com>
2023-08-29 17:09:49 -0400

    import getClassResult and assign to variable. change placeholder comments for handleClassPodium to be key values of class from classResults

--- Socket_Test/getFinal.js
+++ Socket_Test/getFinal.js
@@ -1,26 +1,29 @@
 import { getClasses } from "./functions/entryFuncs";
-import { handleClassPodium } from "./functions/resultFunc";
+import { getClassResults, handleClassPodium } from "./functions/resultFunc";
 
 //** Once command goes to C(?) for checkered, push final messages into an arr, once list === length of entry list */
 const getFinal = (resultArr, series) => {
     // return arr of classes based on series
     const classes = getClasses(series);
 
+    //** assuming resultArr is array of entries for current session, sort by class and return object with arr of class entries */
+    const classResults = getClassResults(resultArr, classes);
+
     const finResult = {
         series,
         date,
         event,
 
-        // handle class will take the top 3 entries per class, and the class
-        result1: handleClassPodium(/* pass object of result */),
+        // handle class will take arr of results per class
+        result1: handleClassPodium(classResults[classes[0]]),
         ...(result2
-            ? { result2: { ...handleClassPodium(/* pass object of result */) } }
+            ? { result2: { ...handleClassPodium(classResults[classes[1]]) } }
             : {}),
         ...(result3
-            ? { result3: { ...handleClassPodium(/* pass object of result */) } }
+            ? { result3: { ...handleClassPodium(classResults[classes[2]]) } }
             : {}),
         ...(result4
-            ? { result4: { ...handleClassPodium(/* pass object of result */) } }
+            ? { result4: { ...handleClassPodium(classResults[classes[3]]) } }
             : {}),
 
         // hard charger
