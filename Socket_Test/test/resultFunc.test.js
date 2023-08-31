cf86fb6c7593f41c251087dfa7c0ffae9ff8e111
Brendan Woo <woo.brendan@gmail.com>
2023-08-31 12:34:01 -0400

    create test for hardcharger, importing entries and cleanEntry

--- Socket_Test/test/resultFunc.test.js
+++ Socket_Test/test/resultFunc.test.js
@@ -1,5 +1,7 @@
 import { assert } from "chai";
-import { formatDate } from "../functions/resultFunc";
+import { formatDate, formatHardCharger } from "../functions/resultFunc.js";
+import entries from "./testEntries.js";
+import { cleanEntry } from "../functions/entryFuncs.js";
 
 describe("Format incoming TZ message to get date", () => {
     it('Should return in format of "08-30-2023"', () => {
@@ -13,8 +15,33 @@ describe("Format incoming TZ message to get date", () => {
         };
 
         const date = formatDate(socketMsg);
-        console.log(date);
 
         assert.equal(date, "5-20-2023");
+    });
+});
+
+describe("Format Award entries, Fast Lap & Hard Charger", () => {
+    it("Should format the hard charger from incoming message", () => {
+        const entry = cleanEntry(entries[0]);
+
+        const expected = {
+            entryNum: "28",
+            startPos: 9,
+            gain: 7,
+            entry: {
+                series: "FGTWCA",
+                number: "28",
+                vehicle: "Porsche GT3 R 992",
+                team: "RS1",
+                driver1: "Eric Filgueiras",
+                driver2: "Stevan McAleer",
+            },
+        };
+
+        // refactor later to gather startPos and gain from messages
+        const hardCharger = formatHardCharger(entry, 9, 7);
+
+        console.log(hardCharger);
+        assert.deepEqual(hardCharger, expected);
     });
 });
