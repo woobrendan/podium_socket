655daf76b29a560b22aa4d2f2b2527fb7d082bdd
Brendan Woo <woo.brendan@gmail.com>
2023-09-01 15:28:57 -0400

    fill in tests for podiums with only 1 or 2 entrants

--- Socket_Test/test/resultFunc.test.js
+++ Socket_Test/test/resultFunc.test.js
@@ -113,7 +113,50 @@ describe("Creating results for class finishers", () => {
         assert.deepEqual(podium, expected);
     });
 
-    it("Should handle podiums and classes that have 2 entrants", () => {});
-    it("Should handle podiums and classes that have 1 entrants", () => {});
+    it("Should handle podiums and classes that have 2 entrants", () => {
+        const cleanEntries = [entries[0], entries[1]].map((entry) =>
+            cleanEntry(entry),
+        );
+
+        const podium = handleClassPodium(cleanEntries);
+
+        const expected = {
+            class: "Pro",
+            firstPlace: {
+                driver1: "Eric Filgueiras",
+                driver2: "Stevan McAleer",
+                vehicle: "Porsche GT3 R 992",
+                team: "RS1",
+                number: "28",
+            },
+            secondPlace: {
+                driver1: "Manny Franco",
+                driver2: "Alessandro Balzan",
+                vehicle: "Ferrari 296 GT3",
+                team: "Conquest Racing",
+                number: "21",
+            },
+        };
+
+        assert.deepEqual(podium, expected);
+    });
+    it("Should handle podiums and classes that have 1 entrants", () => {
+        const cleanEntries = [entries[0]].map((entry) => cleanEntry(entry));
+
+        const podium = handleClassPodium(cleanEntries);
+
+        const expected = {
+            class: "Pro",
+            firstPlace: {
+                driver1: "Eric Filgueiras",
+                driver2: "Stevan McAleer",
+                vehicle: "Porsche GT3 R 992",
+                team: "RS1",
+                number: "28",
+            },
+        };
+
+        assert.deepEqual(podium, expected);
+    });
     it("Should handle podiums and classes that have 3 drivers", () => {});
 });
