8f1e332ab8bc73c3172c13cfe1aceb81b62669d0
Brendan Woo <woo.brendan@gmail.com>
2023-09-04 17:30:00 -0400

    add getSeries test for all other sro series

--- Socket_Test/test/resultFunc.test.js
+++ Socket_Test/test/resultFunc.test.js
@@ -24,7 +24,7 @@ describe("Format incoming socket messages ", () => {
         assert.equal(date, "5-20-2023");
     });
 
-    it("Should take in a command R message and return the series name stripped of extra characters", () => {
+    it("Should take in a command R message and return the GTWCA name stripped of extra characters", () => {
         const message = {
             command: "$R",
             type: "R",
@@ -39,6 +39,54 @@ describe("Format incoming socket messages ", () => {
         const series = getSeries(message);
 
         assert.equal(series, "GT World Challenge America");
+    });
+
+    it("Should take in a command R message and return the PGT4A name stripped of extra characters", () => {
+        const message = {
+            command: "$R",
+            type: "R",
+            eventName: "SRO COTA",
+            eventShortName: "",
+            runName: "Pirelli GT4 America Race 1",
+            runType: "R",
+            _key: "$R",
+        };
+
+        const series = getSeries(message);
+
+        assert.equal(series, "Pirelli GT4 America");
+    });
+
+    it("Should take in a command R message and return the GTAM name stripped of extra characters", () => {
+        const message = {
+            command: "$R",
+            type: "R",
+            eventName: "SRO COTA",
+            eventShortName: "",
+            runName: "GT America Race 2",
+            runType: "R",
+            _key: "$R",
+        };
+
+        const series = getSeries(message);
+
+        assert.equal(series, "GT America");
+    });
+
+    it("Should take in a command R message and return the TCAM name stripped of extra characters", () => {
+        const message = {
+            command: "$R",
+            type: "R",
+            eventName: "SRO COTA",
+            eventShortName: "",
+            runName: "TC America Race 2",
+            runType: "R",
+            _key: "$R",
+        };
+
+        const series = getSeries(message);
+
+        assert.equal(series, "TC America");
     });
 });
 
