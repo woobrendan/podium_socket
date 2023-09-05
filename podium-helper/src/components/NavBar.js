a3e6d055832a3848e4b05168bc437384eedb8d60
Brendan Woo <woo.brendan@gmail.com>
2023-09-05 12:45:45 -0400

    remove entry manager and competitors link inside header

--- podium-helper/src/components/NavBar.js
+++ podium-helper/src/components/NavBar.js
@@ -5,23 +5,24 @@ import "../Styling/navBar.scss";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }} className="nav-bar">
-      <AppBar position="static" sx={{ bgcolor: "#FF0000" }}>
-        <Toolbar>
-          <div className="navbar-links">
-            <a className="img-container" href="https://www.sro-america.com/">
-              <img src={SRO} alt="SRO" />
-            </a>
-            <Link to="/">New Podium</Link>
-            <Link to="/Competitors">Competitors</Link>
-            <Link to="/Results">Results</Link>
-            <Link to="/entryManager">Admin</Link>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }} className="nav-bar">
+            <AppBar position="static" sx={{ bgcolor: "#FF0000" }}>
+                <Toolbar>
+                    <div className="navbar-links">
+                        <a
+                            className="img-container"
+                            href="https://www.sro-america.com/"
+                        >
+                            <img src={SRO} alt="SRO" />
+                        </a>
+                        <Link to="/">New Podium</Link>
+                        <Link to="/Results">Results</Link>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 };
 
 export default NavBar;
