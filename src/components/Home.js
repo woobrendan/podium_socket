4a7cb9d7bb1e3c6c152e75ecebce3a9f2e0c5786
Brendan Woo <woo.brendan@gmail.com>
2023-09-07 16:51:18 -0400

    create variable links for race and results, create loop to return cards for each link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,28 @@
-import { Card } from "@mui/material";
+import { Card, CardContent } from "@mui/material";
+import { useNavigate } from "react-router";
 
-const home = () => {
+const links = [
+    { display: "Live Race", link: "/live" },
+    { display: "Results", link: "/Results" },
+];
+
+const HomePage = () => {
+    const navigate = useNavigate();
+
     return (
         <section id="homepage">
             <div className="nav_list">
-                <Card></Card>
+                <p>Hello</p>
+                {links.map((link, index) => (
+                    <Card key={index} onClick={() => navigate(link.link)}>
+                        <CardContent>
+                            <h1>{link.display}</h1>
+                        </CardContent>
+                    </Card>
+                ))}
             </div>
         </section>
     );
 };
 
-export default home;
+export default HomePage;
