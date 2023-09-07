b330fdd58462ecdee21d70b5dc480a1048ecacb9
Brendan Woo <woo.brendan@gmail.com>
2023-09-07 16:26:04 -0400

    create component for home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import { Card } from "@mui/material";
+
+const home = () => {
+    return (
+        <section id="homepage">
+            <div className="nav_list">
+                <Card></Card>
+            </div>
+        </section>
+    );
+};
+
+export default home;
