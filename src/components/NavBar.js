04b58ae3f433ea012984cad297ff44908c7375dd
Brendan Woo <woo.brendan@gmail.com>
2023-09-08 17:47:50 -0400

    import uselocation, and add conditional to navbar div to render links only if not homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Box, Toolbar, AppBar } from "@mui/material";
 import SRO from "../images/SRO.jpg";
 import "../Styling/navBar.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+    const location = useLocation();
     return (
         <Box sx={{ flexGrow: 1 }} className="nav-bar">
             <AppBar position="static" sx={{ bgcolor: "#FF0000" }}>
@@ -16,9 +17,13 @@ const NavBar = () => {
                         >
                             <img src={SRO} alt="SRO" />
                         </a>
-                        <Link to="/">Home</Link>
-                        <Link to="/live">Live Race</Link>
-                        <Link to="/Results">Results</Link>
+                        {location.pathname !== "/" && (
+                            <>
+                                <Link to="/">Home</Link>
+                                <Link to="/live">Live Race</Link>
+                                <Link to="/Results">Results</Link>
+                            </>
+                        )}
                     </div>
                 </Toolbar>
             </AppBar>
