b248119c7d8f1f7e075bca6820a82b2c95e2d178
Brendan Woo <woo.brendan@gmail.com>
2023-09-11 16:05:19 -0400

    uncomment results and recent podium routes, remove podium component import and comment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./Styling/App.scss";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-// import Podium from "./components/Podium_Creation/Podium.js";
 import NavBar from "./components/NavBar.js";
-// import Results from "./components/Results/ResultsHistory.js";
-// import RecentPodium from "./components/Results/RecentPodium.js";
+import Results from "./components/Results/ResultsHistory.js";
+import RecentPodium from "./components/Results/RecentPodium.js";
 import USACWebSocket from "./components/websocket.js";
 import HomePage from "./components/HomePage.js";
 
@@ -15,9 +14,8 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/live" element={<USACWebSocket />} />
-                    {/* <Route path="/" element={<Podium />} />
-                        <Route path="/results" element={<Results />} />
-                        <Route path="/recent" element={<RecentPodium />} />*/}
+                    <Route path="/results" element={<Results />} />
+                    <Route path="/recent" element={<RecentPodium />} />
                 </Routes>
             </div>
         </Router>
