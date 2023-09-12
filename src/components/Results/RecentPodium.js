9d5c481482623443aeebc05972ff7e7663b16ba3
Brendan Woo <woo.brendan@gmail.com>
2023-09-12 16:35:40 -0400

    remove fetch data, remove dispatch, remove conditional render for resultTableHeader

--- src/components/Results/RecentPodium.js
+++ src/components/Results/RecentPodium.js
@@ -1,27 +1,25 @@
 import ResultTableHeader from "./ResultTableHeader.js";
-import { useEffect } from "react";
 import { printPage } from "../../functions/helperFunc.js";
 import { Button } from "@mui/material";
 import "../../Styling/result.scss";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchData } from "../../store/resultsActions.js";
+import { useSelector } from "react-redux";
 
 const RecentPodium = () => {
-  const dispatch = useDispatch();
-  const recentResult = useSelector((state) => state.results.recentPodium);
+    const recentResult = useSelector((state) => state.results.recentPodium);
 
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
-
-  return (
-    <div className="recent-podium-container">
-      <Button onClick={() => printPage()} variant="contained" color="success">
-        Print Page
-      </Button>
-      {recentResult && <ResultTableHeader results={recentResult} />}
-    </div>
-  );
+    return (
+        <div className="recent-podium-container">
+            <Button
+                onClick={() => printPage()}
+                variant="contained"
+                color="success"
+            >
+                Print Page
+            </Button>
+            <ResultTableHeader results={recentResult} />
+            {/* {recentResult && <ResultTableHeader results={recentResult} />} */}
+        </div>
+    );
 };
 
 export default RecentPodium;
