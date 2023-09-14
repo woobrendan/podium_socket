f1db1ca9414b7a31b6b82a004a9f85e049a1254b
Brendan Woo <woo.brendan@gmail.com>
2023-09-14 13:01:05 -0400

    create component fofr schedule, and add in schedueled events for 2023

---
+++ src/components/FooterSchedule.js
@@ -0,0 +1,17 @@
+const schedule = [
+    { event: "St. Petersburg", date: "Mar 3-5, 2023" },
+    { event: "Sonoma Raceway", date: "Mar 30 - Apr 2, 2023" },
+    { event: "NOLA Motorsports Park", date: "Apr 28-30, 2023" },
+    { event: "Circuit of the Americas", date: "May 19-21, 2023" },
+    { event: "VIRginia International Raceway", date: "June 16-18, 2023" },
+    { event: "Music City Grand Prix", date: "Aug 4-6, 2023" },
+    { event: "Road America", date: "Aug 18-20, 2023" },
+    { event: "Sebring International Raceway", date: "Sept 22-24, 2023" },
+    { event: "Indianapolis Motor Speedway", date: "Oct 5-8, 2023" },
+];
+
+const FooterSchedule = () => {
+    return <> </>;
+};
+
+export default FooterSchedule;
