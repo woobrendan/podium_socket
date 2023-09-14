a0333553abd2452ac855c8ade505c8cbf19b45db
Brendan Woo <woo.brendan@gmail.com>
2023-09-14 14:45:40 -0400

    add track mapes for each race event

--- src/components/Schedule/schedule_info.js
+++ src/components/Schedule/schedule_info.js
@@ -4,14 +4,46 @@ const schedule = [
         date: "Mar 3-5, 2023",
         img: "https://digbza2f4g9qo.cloudfront.net/-/media/IndyCar/Schedules/TrackOutlines/Black/StPetersburg.png?h=240&iar=0&w=300&vs=1&d=20151012T192253Z",
     },
-    { event: "Sonoma Raceway", date: "Mar 30 - Apr 2, 2023" },
-    { event: "NOLA Motorsports Park", date: "Apr 28-30, 2023" },
-    { event: "Circuit of the Americas", date: "May 19-21, 2023" },
-    { event: "VIRginia International Raceway", date: "June 16-18, 2023" },
-    { event: "Music City Grand Prix", date: "Aug 4-6, 2023" },
-    { event: "Road America", date: "Aug 18-20, 2023" },
-    { event: "Sebring International Raceway", date: "Sept 22-24, 2023" },
-    { event: "Indianapolis Motor Speedway", date: "Oct 5-8, 2023" },
+    {
+        event: "Sonoma Raceway",
+        date: "Mar 30 - Apr 2, 2023",
+        img: "https://www.trackdecals.com/cdn/shop/products/Long.image_c2bee7d4-3edd-4f04-ba6b-4887110fcb46.jpg?v=1594322949",
+    },
+    {
+        event: "NOLA Motorsports Park",
+        date: "Apr 28-30, 2023",
+        img: "https://www.trackdecals.com/cdn/shop/products/Indy.image_1200x1200.jpg?v=1594398624",
+    },
+    {
+        event: "Circuit of the Americas",
+        date: "May 19-21, 2023",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ9nh9xc7UymDCmvYVRzlF8Nfs0HU438obmQ&usqp=CAU",
+    },
+    {
+        event: "VIRginia International Raceway",
+        date: "June 16-18, 2023",
+        img: "https://cdn.connectsites.net/user_files/f4/events/001/993/042/original.jpg?1544213480&height=1200&width=1200",
+    },
+    {
+        event: "Music City Grand Prix",
+        date: "Aug 4-6, 2023",
+        img: "https://digbza2f4g9qo.cloudfront.net/-/media/IndyCar/Schedules/TrackOutlines/Black/Nashville.png?h=240&iar=0&w=300&vs=1&d=20200930T222750Z",
+    },
+    {
+        event: "Road America",
+        date: "Aug 18-20, 2023",
+        img: "https://digbza2f4g9qo.cloudfront.net/-/media/IndyCar/Schedules/TrackOutlines/Black/RoadAmerica.png?h=240&iar=0&w=300&vs=1&d=20151012T205328Z",
+    },
+    {
+        event: "Sebring International Raceway",
+        date: "Sept 22-24, 2023",
+        img: "https://cdn.connectsites.net/user_files/f4/events/001/995/585/original.jpg?1576608685&height=1200&width=1200",
+    },
+    {
+        event: "Indianapolis Motor Speedway",
+        date: "Oct 5-8, 2023",
+        img: "https://media.teampenske.com/assets/trackmap-indianapolis-road.png",
+    },
 ];
 
 export default schedule;
